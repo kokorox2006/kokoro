dc3f812025bef2fd871dab7f8238658561e80609
kokorox2006 <kokorox2006@gmail.com>
2025-03-29 21:03:49 +0530

    Add Component

---
+++ src/components/Publish.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Publish = () => {
+  return (
+    <>
+      <h1>Publish componet</h1>
+    </>
+  );
+};
+
+export default Publish;
---
+++ src/components/Showcase.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Showcase = () => {
+  return (
+    <>
+      <h1>Showcase Componet</h1>
+    </>
+  );
+};
+
+export default Showcase;
