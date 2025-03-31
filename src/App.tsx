5603a0d779ba1fb405b1c5949171b551923f9473
Rupesh Pathak <mr.rupeshpathak@outlook.com>
2025-03-31 15:31:55 +0530

    Add anchor tags to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar.tsx";
+import Navbar from "./components/Navbar";
 import React from "react";
 
 const App = () => {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,24 @@ const Navbar = () => {
   return (
     <>
       <header className="w-full flex justify-center items-center">
-        <nav className=" flex gap-64 justify-between items-center mr-auto ml-auto pt-4 pb-4">
+        <nav className=" flex justify-between items-center gap-64 pt-4 pb-4">
           <div className="logo flex gap-2 items-center ">
             <img src="src/assets/logo.png" alt="" className="w-8" />
             <p className="text-2xl text-white font-medium">kokoro</p>
           </div>
           <ul className="flex gap-8 text-white">
-            <li>Public notes</li>
-            <li>Publish</li>
-            <li>Community</li>
-            <li>GitHub</li>
+            <li>
+              <a href="#">Public notes</a>
+            </li>
+            <li>
+              <a href="#">Publish</a>
+            </li>
+            <li>
+              <a href="#">Community</a>
+            </li>
+            <li>
+              <a href="#">GitHub</a>
+            </li>
           </ul>
           <button className="bg-blue-500 pl-4 pr-4 pt-2 pb-2 text-white font-semibold rounded-md bg-gradient-to-b">
             Sign up
