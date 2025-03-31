95ab51d2865147dd9bfa9b97d3cdf9a81606da6c
Rupesh Pathak <mr.rupeshpathak@outlook.com>
2025-03-31 13:50:15 +0530

    Fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,23 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="pl-32 pr-32 pt-5 pb-5 flex justify-between items-center">
-        <div className="logo flex gap-2 items-center ">
-          <img src="src/assets/logo.png" alt="" className="w-8" />
-          <p className="text-2xl text-white font-medium">kokoro</p>
-        </div>
-        <ul className="flex gap-8 text-white">
-          <li>Public notes</li>
-          <li>Publish</li>
-          <li>Community</li>
-          <li>GitHub</li>
-        </ul>
-        <button className="bg-blue-500 pl-4 pr-4 pt-2 pb-2 text-white font-semibold rounded-md bg-gradient-to-b">
-          Sign up
-        </button>
-      </nav>
+      <header className="w-full flex justify-center items-center">
+        <nav className=" flex gap-64 justify-between items-center mr-auto ml-auto pt-4 pb-4">
+          <div className="logo flex gap-2 items-center ">
+            <img src="src/assets/logo.png" alt="" className="w-8" />
+            <p className="text-2xl text-white font-medium">kokoro</p>
+          </div>
+          <ul className="flex gap-8 text-white">
+            <li>Public notes</li>
+            <li>Publish</li>
+            <li>Community</li>
+            <li>GitHub</li>
+          </ul>
+          <button className="bg-blue-500 pl-4 pr-4 pt-2 pb-2 text-white font-semibold rounded-md bg-gradient-to-b">
+            Sign up
+          </button>
+        </nav>
+      </header>
     </>
   );
 };
